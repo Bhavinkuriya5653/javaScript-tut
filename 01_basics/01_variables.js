b085cf92226fe0fcdc35d0a6fb4b06c105343a4a
bhavink <113357380+Bhavinkuriya5653@users.noreply.github.com>
2023-09-08 00:22:04 +0530

    basic Knowledge of variables
    -

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const userAccountId = 189;
+let userEmail = "javascript@yopmail.com";
+var userPassword = "12345678";
+
+userCityName = "unitedState";
+userEmail = "newjavaScript@yopmail.com";
+userPassword = "let@123";
+userCityName = "Austin";
+let userAccoutDetails; // undefined
+
+// userAccountId = 178;  not allowed
+
+/* 
+prefer not to use var  
+beacause of block of scope and functional scope 
+*/
+
+console.log("--> ", userAccountId);
+console.table([userEmail, userPassword, userCityName, userAccoutDetails]);
