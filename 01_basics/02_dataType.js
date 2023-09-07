3b974ed305c1a9d70530b0773d990d9fa659d560
bhavink <113357380+Bhavinkuriya5653@users.noreply.github.com>
2023-09-08 00:49:37 +0530

    basic of data type

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as a newer version
+
+let name = "jenny";
+let number = 3;
+let isLoggedIn = false;
+
+// ===> primitive data type
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null   it's actually standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// ==> object
+
+// console.log(typeof null); // giving type object
+// console.log(typeof undefined); // giving type undefined
