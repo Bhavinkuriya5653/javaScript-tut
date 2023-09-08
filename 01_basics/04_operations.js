2d2375ac01023cc09d63b0562c66b3a928215a41
bhavink <113357380+Bhavinkuriya5653@users.noreply.github.com>
2023-09-08 12:10:45 +0530

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,22 @@
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+// console.log(3 + 4);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "hello ";
+let str2 = "developers";
+
+console.log(str1 + str2);
+
+// magic happen here
+// its only use for knowledge do not try to implement in real life projects bcz its messy
+console.log(1 + 2 + "2"); // 32
+console.log("1" + 2 + 3); // 123
+console.log("1" + 2); // number 12
+console.log("1" + "2"); // number 12
